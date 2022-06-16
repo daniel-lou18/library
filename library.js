b252eadc0a94e7f7da56f73ef9407410700b8961
DanielLou <danielderudder@hotmail.com>
2022-06-16 12:35:17 +0200

    Add 'delete' and 'change read status' buttons

--- library.js
+++ library.js
@@ -27,31 +27,45 @@ function displayLibrary() {
     const author = document.createElement('td');
     const pages = document.createElement('td');
     const read = document.createElement('td');
+    const removeBook = document.createElement('button');
+    const toggleRead = document.createElement('button');
 
     title.textContent = book.title;
     author.textContent = book.author;
     pages.textContent = book.pages;
     read.textContent = book.read;
+    removeBook.textContent = 'Remove';
+    toggleRead.textContent = 'Change status';
 
     row.setAttribute('id', `row-${index}`);
+    row.setAttribute('data-row-num', `${index}`)
     title.setAttribute('id', `title-${index}`);
     author.setAttribute('id', `author-${index}`);
     pages.setAttribute('id', `pages-${index}`);
     read.setAttribute('id', `read-${index}`);
+    removeBook.setAttribute('data-row-num', `${index}`);
+    toggleRead.setAttribute('data-row-num', `${index}`);
+
+    removeBook.addEventListener('click', deleteBook);
+    toggleRead.addEventListener('click', toggleStatus);
 
     tbody.appendChild(row);
     const tr = document.querySelector(`#row-${index}`);
     tr.appendChild(title);
     tr.appendChild(author);
     tr.appendChild(pages);
-    tr.appendChild(read);    
+    tr.appendChild(read);
+    tr.appendChild(removeBook);
+    tr.appendChild(toggleRead);
   })
 }
 
 const siddhartha = new Book('Siddhartha', 'Hermann Hesse', 300, true);
 const aneantir = new Book('Anéantir', 'Michel Houellebecq', 500, false);
+const gallico = new Book('De bello gallico', 'Julius Caesar', 150, true);
 addBookToLibrary(siddhartha);
 addBookToLibrary(aneantir);
+addBookToLibrary(gallico);
 displayLibrary();
 
 // const newBook = document.querySelector('.add-book');
@@ -66,10 +80,20 @@ function addBook(e) {
   const author = document.querySelector('input[name="author"]');
   const pages = document.querySelector('input[name="pages"]');
   const read = document.querySelector('input[name="read"]:checked');
-  const book = new Book(title.value, author.value, pages.value, read.value);
+  const book = new Book(title.value, author.value, pages.value, Boolean(read.value));
   addBookToLibrary(book);
   console.log(myLibrary);
   displayLibrary();
   e.preventDefault();
+};
+
+function deleteBook(e) {
+  myLibrary.splice(`${e.target.dataset.rowNum}`, 1);
+  displayLibrary();
+};
+
+function toggleStatus(e) {
+  myLibrary[`${e.target.dataset.rowNum}`].read = !myLibrary[`${e.target.dataset.rowNum}`].read;
+  displayLibrary();
 }
 
